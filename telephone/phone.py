8f47cbba875e0ac9f78d4afb9ec80e08179fa8fe
zipei wei <ironmanzpw@zipeis-MacBook-Pro.local>
2019-09-05 18:14:24 -0400

    create dictionary for number to word

--- telephone/phone.py
+++ telephone/phone.py
@@ -5,25 +5,45 @@ from enchant.tokenize import get_tokenizer
 from input import *
 
 Phone_Directory = {
-				 '0': '',
-     			 '1': '',
-				 '2': ['a', 'b', 'c'],
-                 '3': ['d', 'e', 'f'],
-                 '4': ['g', 'h', 'i'],
-                 '5': ['j', 'k', 'l'],
-                 '6': ['m', 'n', 'o'],
-                 '7': ['p', 'q', 'r', 's'],
-                 '8': ['t', 'u', 'v'],
-                 '9': ['w', 'x', 'y', 'z']}
+				 '2': ['A', 'B', 'C'],
+                 '3': ['D', 'E', 'F'],
+                 '4': ['G', 'G', 'I'],
+                 '5': ['J', 'K', 'L'],
+                 '6': ['M', 'N', 'O'],
+                 '7': ['P', 'Q', 'R', 'S'],
+                 '8': ['T', 'U', 'V'],
+                 '9': ['W', 'X', 'Y', 'Z']}
+
+Check_Word = enchant.Dict("en_US")
 
 class Telephone_Book:
     def __init__(self, string):
         self.string = string
 
+    def get_word(sub_string):
+        return Check_Word.check(sub_string)
+
+
+    def all_wordifications(self):
+        # decode the string by removing all unnecessary chars
+        clean_words = self.decoding()
+        # if the string input is not a telephone, exit program
+        if not clean_words:
+            sys.exit("Wrong Input len or char, retry")
+    
+
+    
+
+
+
+
+
+
+
     def decoding(self):
         x = re.sub('[\W_]+', '', self.string)
         return x if len(x) == 10 or len(x) == 11 else False
-    
+
 
     def encoding_numbers(self, raw_string):
         # 876-678-1000
