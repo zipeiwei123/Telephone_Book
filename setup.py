56f7269f87e0e5d0176eceb1bc7be6d9f2edb2e7
zipeiwei123 <zipeiwei123@gmail.com>
2019-09-06 06:31:20 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,16 @@
 from distutils.core import setup
 
-with open("README.md", "r") as fh:
-    l_description = fh.read()
 
 setup(
   name = 'phonewordzp',         
   packages = ['phonewordzp'],   
-  version = 'v0.8',      
+  version = 'v0.9',      
   license='MIT',        
   description = 'A Python Class to convert numbers to English Words, English Words to phone numbers. Currently support 10-11 phone numbers',   # Give a short description about your library
   author = 'Zipei Wei',                   
   author_email = 'zipeiwei123@gmail.com',      
   url = 'https://github.com/zipeiwei123/Telephone_Book',
-  long_description=l_description,
-  long_description_content_type="text/markdown",   
-  download_url = 'https://github.com/zipeiwei123/Telephone_Book/archive/v0.8.tar.gz',    
+  download_url = 'https://github.com/zipeiwei123/Telephone_Book/archive/v0.9.tar.gz',    
   keywords = ['Phoneword', 'Python', 'number_to_words'],   
   
   classifiers=[
