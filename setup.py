4b20bad8976377b3186c5981708ff08411f65fcd
zipeiwei123 <zipeiwei123@gmail.com>
2019-09-05 23:47:16 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 setup(
-  name = 'telepy',         # How you named your package folder (MyLib)
+  name = 'phonewordzp',         # How you named your package folder (MyLib)
   packages = ['telepy'],   # Chose the same as "name"
-  version = 'v0.3',      # Start with a small number and increase it with every change you make
+  version = 'v0.5',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A Python Class to convert numbers to English Words, English Words to phone numbers. Currently support 10-11 phone numbers',   # Give a short description about your library
   author = 'Zipei Wei',                   # Type in your name
   author_email = 'zipeiwei123@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/zipeiwei123/Telephone_Book',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/zipeiwei123/Telephone_Book/archive/v0.4.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/zipeiwei123/Telephone_Book/archive/v0.5.tar.gz',    # I explain this later on
   keywords = ['Phoneword', 'Python', 'number_to_words'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           're',
