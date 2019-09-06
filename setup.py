bf21cd04474ce1be816d242bb5edf0f5feea4930
zipeiwei123 <zipeiwei123@gmail.com>
2019-09-06 06:04:03 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,28 @@
 from distutils.core import setup
+
+with open("README.md", "r") as fh:
+    l_description = fh.read()
+
 setup(
-  name = 'phonewordzp',         # How you named your package folder (MyLib)
-  packages = ['phonewordzp'],   # Chose the same as "name"
-  version = 'v0.5',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  name = 'phonewordzp',         
+  packages = ['phonewordzp'],   
+  version = 'v0.6',      
+  license='MIT',        
   description = 'A Python Class to convert numbers to English Words, English Words to phone numbers. Currently support 10-11 phone numbers',   # Give a short description about your library
-  author = 'Zipei Wei',                   # Type in your name
-  author_email = 'zipeiwei123@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/zipeiwei123/Telephone_Book',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/zipeiwei123/Telephone_Book/archive/v0.5.tar.gz',    # I explain this later on
-  keywords = ['Phoneword', 'Python', 'number_to_words'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-          're',
-          'enchant',
-      ],
+  author = 'Zipei Wei',                   
+  author_email = 'zipeiwei123@gmail.com',      
+  url = 'https://github.com/zipeiwei123/Telephone_Book',
+  long_description=l_description,
+  long_description_content_type="text/markdown",   
+  download_url = 'https://github.com/zipeiwei123/Telephone_Book/archive/v0.6.tar.gz',    
+  keywords = ['Phoneword', 'Python', 'number_to_words'],   
+  
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license,   # Again, pick a license
-    'Programming Language :: Python :: 3.6',
-  ],
-)
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.4",
+          "Programming Language :: Python :: 3.6",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+      ],
+  include_package_data=True)
